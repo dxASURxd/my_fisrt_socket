b4968c10f8a316a0cb1d914bca09230722b4abb3
asur <asur_xd@hotmail.com>
2024-05-10 15:07:54 -0600

    cors

--- models/server.js
+++ models/server.js
@@ -3,6 +3,7 @@ const http = require('http');
 const socketio = require('socket.io');
 const path = require('path');
 const Sockets = require('./sockets');
+const cors = require('cors');
 
 class Server {
     constructor() {
@@ -19,6 +20,8 @@ class Server {
     middlewares() {
         // Desplegar el directorio público
         this.app.use( express.static( path.resolve(__dirname, '../public') ) );
+        
+        this.app.use( cors());
     }
 
     configurarSockets(){
